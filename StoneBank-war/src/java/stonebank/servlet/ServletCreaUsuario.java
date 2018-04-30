593ae1d43d8e5837de1c14ec8e4ea0df8fb417e1
Takox <Takox@Takox-PC>
2018-04-30 18:01:11 +0200

    alta.jsp y ServletCrearUusuario

--- StoneBank-war/src/java/stonebank/servlet/ServletCreaUsuario.java
+++ StoneBank-war/src/java/stonebank/servlet/ServletCreaUsuario.java
@@ -7,41 +7,73 @@ package stonebank.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import stonebank.ejb.TusuarioFacade;
+import stonebank.entity.Trol;
+import stonebank.entity.Tusuario;
 
 /**
  *
- * @author rafaelpernil
+ * @author Fran Gambero
  */
 public class ServletCreaUsuario extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    @EJB
+    private TusuarioFacade tusuarioFacade;
+    
+    
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ServletCreaUsuario</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ServletCreaUsuario at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        
+        String nombre, apellido, contrasena, email, domicilio;
+        int dni,telefono;
+        Tusuario usuario;
+        
+        nombre = request.getParameter("nombre");
+        apellido = request.getParameter("apellido");
+        dni = Integer.parseInt(request.getParameter("dni"));
+        contrasena = request.getParameter("contrasena");
+        telefono = Integer.parseInt(request.getParameter("telefono"));
+        email = request.getParameter("email");
+        domicilio = request.getParameter("domicilio");
+        Trol trol = new Trol(1);
+        /* Esto se usará cuando se implementa crear y editar aquí
+        if("".equals(id)){ //Nuevo usuario
+            usuario = new Tusuario();
+        }else{ //Editar usuario
+            usuario = this.tusuarioFacade.find(dni);
         }
+        */
+        
+        /*if(!"".equals(dni)){ //Solo sirve para crear usuarios, asegura que existe dni
+            usuario = new Tusuario();
+        } else{
+            System.out.println("Error en usuario");
+        } */
+        
+        usuario = new Tusuario();
+        
+        usuario.setNombre(nombre);
+        usuario.setApellidos(apellido);
+        usuario.setDniUsuario(dni);
+        usuario.setTelefono(telefono);
+        usuario.setEmail(email);
+        usuario.setDomicilio(domicilio);
+        usuario.setHashContrasena(contrasena);
+        usuario.setTrolIdtrol(trol);
+        usuario.setNumCuenta(11);
+        
+        this.tusuarioFacade.create(usuario);
+        
+        RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/login.jsp");
+        rd.forward(request, response);  
+        
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
---
+++ StoneBank-war/web/alta.jsp
@@ -0,0 +1,53 @@
+<%-- 
+    Document   : alta
+    Created on : 30-abr-2018, 16:52:58
+    Author     : Fran Gambero
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Alta Cliente</title>
+    </head>
+    <body>
+        <div align="center">
+        <h1>Crear nuevo cliente</h1>
+        <form action="ServletCreaUsuario" method="post">
+            <table>
+                <tr>
+                    <td>Nombre: </td>
+                    <td><input type="text" name="nombre" max="30" maxlength="30"/></td>                    
+                </tr>
+                <tr>
+                    <td>Apellido: </td>
+                    <td><input type="text" name="apellido"/></td>                    
+                </tr>
+                <tr>
+                    <td>DNI: </td>
+                    <td><input type="text" name="dni"/></td>                    
+                </tr>
+                <tr>
+                    <td>Contraseña: </td>
+                    <td><input type="text" name="contrasena"/></td>                    
+                </tr>
+                <tr>
+                    <td>Telefono: </td>
+                    <td><input type="text" name="telefono"/></td>                    
+                </tr>
+                <tr>
+                    <td>Email: </td>
+                    <td><input type="text" name="email"/></td>                    
+                </tr>
+                <tr>
+                    <td>Domicilio: </td>
+                    <td><input type="text" name="domicilio"/></td>                    
+                </tr>
+            </table>
+            <br>
+            <input type="submit" value="Crear usuario" />
+        </form>
+        </div>
+    </body>
+</html>
