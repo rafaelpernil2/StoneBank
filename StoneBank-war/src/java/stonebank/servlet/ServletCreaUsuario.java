7765a9a90442ba33afcc728a8c499bacd743def6
Rafael Pernil <rafaelpernil@uma.es>
2018-04-30 20:02:20 +0200

    Added hash for password and account number generator

--- StoneBank-war/src/java/stonebank/servlet/ServletCreaUsuario.java
+++ StoneBank-war/src/java/stonebank/servlet/ServletCreaUsuario.java
@@ -7,41 +7,118 @@ package stonebank.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import stonebank.ejb.TusuarioFacade;
+import stonebank.entity.Trol;
+import stonebank.entity.Tusuario;
 
 /**
  *
- * @author rafaelpernil
+ * @author Fran Gambero
  */
 public class ServletCreaUsuario extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    @EJB
+    private TusuarioFacade tusuarioFacade;
+    
+    
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ServletCreaUsuario</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ServletCreaUsuario at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            throws ServletException, IOException, NoSuchAlgorithmException {
+        
+        String nombre, apellido, contrasena, email, domicilio;
+        int dni,telefono;
+        Tusuario usuario;
+        boolean ready = true;
+        nombre = request.getParameter("nombre");
+        apellido = request.getParameter("apellido");
+        dni = Integer.parseInt(request.getParameter("dni"));
+        contrasena = request.getParameter("contrasena");
+        telefono = Integer.parseInt(request.getParameter("telefono"));
+        email = request.getParameter("email");
+        domicilio = request.getParameter("domicilio");
+        Trol trol = new Trol(1);
+        /* Esto se usará cuando se implementa crear y editar aquí
+        if("".equals(id)){ //Nuevo usuario
+            usuario = new Tusuario();
+        }else{ //Editar usuario
+            usuario = this.tusuarioFacade.find(dni);
         }
+        */
+        
+        /*if(!"".equals(dni)){ //Solo sirve para crear usuarios, asegura que existe dni
+            usuario = new Tusuario();
+        } else{
+            System.out.println("Error en usuario");
+        } */
+        
+        usuario = new Tusuario();
+        
+        usuario.setNombre(nombre);
+        usuario.setApellidos(apellido);
+        //usuario.setDniUsuario(dni);
+        usuario.setTelefono(telefono);
+        usuario.setEmail(email);
+        usuario.setDomicilio(domicilio);
+        
+        //SHA-256 HASH
+        MessageDigest msgdgst = MessageDigest.getInstance("SHA-256");
+        byte[] encodedhash = msgdgst.digest(contrasena.getBytes(StandardCharsets.UTF_8));
+        
+        StringBuilder hexString = new StringBuilder();
+        for (int i = 0; i < encodedhash.length; i++) {
+            String hex = Integer.toHexString(0xff & encodedhash[i]);
+            if(hex.length() == 1) 
+                hexString.append('0');
+            hexString.append(hex);
+        }
+        //
+        usuario.setHashContrasena(hexString.toString());
+        usuario.setTrolIdtrol(trol);
+        // Generacíon número de cuenta
+         List<Tusuario> lista = tusuarioFacade.findAll();
+        int i = 0;
+        Random random = new Random();
+        int rand = Math.abs(random.nextInt());
+        while(i<lista.size()){
+            if (lista.get(i).getNumCuenta() == rand){
+                rand = Math.abs(random.nextInt());
+                i=0;
+            }
+            i++;
+        }
+        usuario.setNumCuenta(rand);
+        
+        i=0;
+        while (i<lista.size() && lista.get(i).getDniUsuario()!= dni)
+            i++;
+        
+        if (i!=lista.size())
+            ready = false;
+               
+        else
+            usuario.setDniUsuario(dni);
+                       
+        
+        if (ready){
+            this.tusuarioFacade.create(usuario);
+            RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/login.jsp");
+            rd.forward(request, response);
+        }
+        
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -56,7 +133,11 @@ public class ServletCreaUsuario extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(ServletCreaUsuario.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -70,7 +151,11 @@ public class ServletCreaUsuario extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(ServletCreaUsuario.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
--- StoneBank-war/web/usuario/indexUsuario.jsp
+++ StoneBank-war/web/usuario/indexUsuario.jsp
@@ -1,17 +1,42 @@
-<%-- 
-    Document   : indexUsuario
-    Created on : 19-abr-2018, 13:24:37
-    Author     : rafaelpernil
---%>
-
-<%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Index Usuario</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1>¡Bievenido/a usuario/a (nombre)!</h1>
+        <hr>
+        
+        <div class = "padre" style="width:100%">
+            
+          <div class = "subdiv-izquierdo" style="width:40%;display:inline-table;">
+            <h3>Últimas transacciones</h3>
+            
+            <table border="1">
+                <tbody>
+                    <tr>
+                        <td>Fecha</td>
+                        <td>Cliente</td>
+                        <td>Concepto</td>
+                        <td>Importe</td>
+                    </tr>
+                </tbody>
+            </table>
+            
+            <br>
+            <a href="ServletListaMovimientos" >Ver todas las transacciones</a>
+          </div>
+            
+          <div class="subdiv-derecho" style="width:40%;display:inline-table;">
+              <h3>Buscador de movimientos</h3>
+              <form action="ServletBusqueda" method="post">
+                    <input type="text" name="Buscador" max="30" maxlength="30" value="Concepto, nombre..."/>
+                    <input type="submit" value="Buscar" />              
+              </form>
+              <br><br>
+              <a href="realizarTransferencia.jsp">Realizar transferecia</a>
+          </div>
+        </div>
     </body>
 </html>
