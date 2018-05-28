436d2ba3fc75710d36f0d361e1df6c68071cf860
Takox <Takox@Takox-PC>
2018-05-28 13:32:30 +0200

    loginFuncionando, error usuarioBean

--- StoneBank-war/src/java/stonebank/bean/UsuarioBean.java
+++ StoneBank-war/src/java/stonebank/bean/UsuarioBean.java
@@ -1,20 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package stonebank.bean;
 
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.SessionScoped;
+
 import javax.inject.Named;
-import javax.enterprise.context.Dependent;
+
+import javax.inject.Inject;
+import stonebank.ejb.TusuarioFacade;
+import stonebank.entity.Tmovimiento;
+import stonebank.entity.Tusuario;
 
 /**
  *
  * @author Victor
  */
 @Named(value = "usuarioBean")
-@Dependent
+@RequestScoped
 public class UsuarioBean {
+
+    @Inject
+    private LoginBean loginBean;
+    
+    @EJB
+    private TusuarioFacade tusuarioFacade;
+    
+    
+    
+    protected List<Tmovimiento> listaMovimientos;
+    protected Tusuario usuario;
+    
+    
 
     /**
      * Creates a new instance of UsuarioBean
@@ -22,4 +40,70 @@ public class UsuarioBean {
     public UsuarioBean() {
     }
     
+    @PostConstruct
+    public void init(){
+        
+        usuario= loginBean.usuarioLoggeado;
+        listaMovimientos = usuario.getTmovimientoList();
+    }
+    
+    /*
+    * hace falta poner el saldo que tiene justo aqui.
+    */
+    
+    public List<Tmovimiento> getListaMovimientos(){
+        return listaMovimientos;
+    }
+    
+    public void setListaMovimientos(List<Tmovimiento> lm){
+        this.listaMovimientos = lm;
+    }
+    
+    public String getNombre(){
+        return usuario.getNombre();
+    }
+    
+    public void setNombre(String n){
+        usuario.setNombre(n);
+    }
+    
+    public String getApellidos(){
+        return usuario.getApellidos();
+    }
+    
+    public void setApellidos(String ap){
+        usuario.setApellidos(ap);
+    }
+    
+    public Integer getDNI(){
+        return usuario.getDniUsuario();
+    }
+    
+    public String getDomicilio(){
+        return usuario.getDomicilio();
+    }
+    
+    public void setDomicilio(String d){
+        usuario.setDomicilio(d);
+    }
+    
+    public String getEmail(){
+        return usuario.getEmail();
+    }
+    
+    public void setEmail(String em){
+        usuario.setEmail(em);
+    }
+    
+    public Integer getTelefono(){
+        return usuario.getTelefono();
+    }
+    
+    public void setTelefono(Integer tel){
+        usuario.setTelefono(tel);
+    }
+    
+    public void setContraseña(){
+        usuario.getHashContrasena();
+    }
 }
